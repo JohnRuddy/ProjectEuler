048a300a1675107a45fcdddd543b87321ecb280c
John Ruddy <johnpruddy@gmail.com>
2015-03-03 08:27:47 +0000

    Added problem 10

---
+++ ProjectEuler.Net/Problem10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Net
+{
+    class Problem10 : IProjectEulerProblem
+    {
+
+        public void Execute()
+        {
+            Int64 primeSum = 0;
+            var length = 2000000;
+
+            for (int i = 2; i <= length; i++)
+            {
+                if (IsPrime(i) || i == 2)
+                {
+                    primeSum = primeSum + i;
+                }
+            }   
+
+            // finally print out the result
+            Console.WriteLine(primeSum);
+        }
+
+        public bool IsPrime(Int64 number)
+        {
+            if (number < 2) return false;
+            if (number % 2 == 0) return (number == 2);
+            int root = (int)Math.Sqrt((double)number);
+            for (int i = 3; i <= root; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
+
+
